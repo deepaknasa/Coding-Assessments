782641f781a05551edc50d4eb601167511fff93b
Deepak Kumar <deepaknasa@gmail.com>
2017-04-29 21:20:59 +1000

    String Builder reference comparison

---
+++ IKMAssessment/CSharp/StringBuilderExample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IKMAssessment.CSharp
+{
+    class StringBuilderExample
+    {
+        public void Init()
+        {
+            String string1 = "one";
+            String string2 = "one";
+            StringBuilder stringBuilder1 = new StringBuilder("one");
+            StringBuilder stringBuilder2 = new StringBuilder("one");
+            List<object> list = new List<object>();
+            list.Add(string1);
+            list.Add(stringBuilder1);
+            Console.WriteLine(list.Contains(string2));
+            Console.WriteLine(list.Contains(stringBuilder2));
+            Console.WriteLine(list.Contains(stringBuilder2.ToString()));
+        }
+    }
+}
